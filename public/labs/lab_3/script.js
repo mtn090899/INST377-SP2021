bea13580d0f435c8de5a96ef4d0474a3d907a5e0
mtn090899 <78068147+mtn090899@users.noreply.github.com>
2021-02-12 13:48:08 -0500

    Lab_3_finish

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,30 @@
-/* Put your javascript in here */
+/* eslint-disable */
+const slideArray = Array.from(document.querySelectorAll('.slide'), function(slide){return slide.dataset.background;
+});
+
+
+let currentSlideIndex = -1;
+
+function setSliderItem(index) {
+  currentSlideIndex = index%slideArray.length;
+
+  if (index < 0){
+    currentSlideIndex = slideArray.length + index;
+  }
+  console.log(slideArray.length);
+  console.log(currentSlideIndex);
+
+  document.querySelector('.slider').style.cssText = 'background: url("' + slideArray[currentSlideIndex] + '") no-repeat center center;';
+
+  const elems = Array.from(document.getElementsByClassName('caption'));
+   elems.forEach(function(elem){
+     elem.style.cssText = 'opacity: 0;'; 
+   
+  });
+
+  const currentCaption = document.querySelector('.caption-' + (currentSlideIndex));
+  currentCaption.style.cssText = 'opacity: 1;';
+}
+
+
+// let intervalID = setInterval(advanceSliderItem, 3000);
