03e1026b9bf3671c0c028e3c56544b29cb06dc5b
mtn090899 <78068147+mtn090899@users.noreply.github.com>
2021-03-12 19:11:04 -0500

    lab 5

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,5 +1,5 @@
 function mapInit() {
-  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  const mymap = L.map('mapid').setView([38.9897, -76.9378], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -22,17 +22,18 @@ async function dataHandler(mapObjectFromFunction) {
   form.addEventListener('submit', async (event) => {
     event.preventDefault();
     console.log('form submitted')
-    const filtered = data.filter((record) => record.zip.includes(search.value));
-    console.log(filtered)
+    const filtered = data.filter((record) => record.zip.includes(search.value) && record.geocoded_column_1);
+    console.table(filtered);
     filtered.forEach((item) => {
       const longLat = item.geocoded_column_1.coordinates;
       console.log('markerLongLat', longLat[0], longLat[1]);
-      const marker = L.marker([longLat[1], longLat[0]]).addTo(mapFromMapFunction);
+      const marker = L.marker([longLat[1], longLat[0]]).addTo(mapObjectFromFunction);
+      
 
       const appendItem = document.createElement('li');
       appendItem.classList.add('block');
       appendItem.classList.add('list-item');
-      appendItem.innerHTML = '<div class= "list-header is-size-5>${item.name}</div><address class="is-size-6">${item.address_line1}</address>';
+      appendItem.innerHTML = `<div class= "list-header is-size-5">${item.name}</div><address class="is-size-6">${item.address_line_1}</address>`;
       targetList.append(appendItem);
     });
 
